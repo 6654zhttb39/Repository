a3e6bd6473dca56b30ba73e7d2d944c7167a9306
6654zhttb39 <2022169962@>
2019-11-28 12:20:44 +0800

    模块 dfgdfg 的初始签入

---
+++ dfgdfg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dfgdfg
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)     //公鸡最多只能有19只
+            {
+                for (int j = 0; j < 33; j++)  //母鸡最多有32只
+                {
+                    if ((100 - i - j) % 3 == 0 && i * 5 + j * 3 + (100 - i - j) / 3 == 100)  //满足花一百块钱，鸡满一百只输出
+                    {
+                        Console.WriteLine("公鸡：{0},母鸡：{1},小鸡：{2}", i, j, 100 - i - j);
+                    }
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
